d817d28700ddb54d1155ec8dbf7c8f1e784cd438
mehran <mohammadmoallemi78@gmail.com>
2024-06-15 01:33:54 +0330

    add all utils functions + types

---
+++ utils/utils.d.ts
@@ -0,0 +1,24 @@
+ interface CarProps {
+    city_mpg: number;
+    class: string;
+    combination_mpg: number;
+    cylinders: number;
+    displacement: number;
+    drive: string;
+    fuel_type: string;
+    highway_mpg: number;
+    make: string;
+    model: string;
+    transmission: string;
+    year: number;
+  }
+
+
+
+  interface FilterProps {
+    manufacturer?: string;
+    year?: number;
+    model?: string;
+    limit?: number;
+    fuel?: string;
+  }
