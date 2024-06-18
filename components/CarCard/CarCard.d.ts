f161603890c81d9eb5a4e7f9c9ca4ce246872425
mehran <mohammadmoallemi78@gmail.com>
2024-06-18 23:01:59 +0330

    style:carCard component + types

---
+++ components/CarCard/CarCard.d.ts
@@ -0,0 +1,24 @@
+interface CarDetailsProps {
+  isOpen: boolean;
+  closeModal: () => void;
+  car: CarProps;
+}
+
+interface CarProps {
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
+}
+
+interface CarCardProps {
+  car: CarProps;
+}
