39111be1c119e8e4dd15880d65f513701d6f0bae
mehran <mohammadmoallemi78@gmail.com>
2024-06-23 00:14:49 +0330

    feat:use classCombiner function instead template string

--- components/Searchbar/SearchButton.tsx
+++ components/Searchbar/SearchButton.tsx
@@ -1,15 +1,19 @@
+'use client'
+import { classCombiner } from "@/utils";
 import Image from "next/image";
 
-export const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
-    <button type='submit' className={`-ml-3 z-10 ${otherClasses}`}>
-      <Image
-        src={"/magnifying-glass.svg"}
-        alt={"magnifying glass"}
-        width={40}
-        height={40}
-        className='object-contain'
-      />
-    </button>
-);
+export const SearchButton = ({ otherClasses }: { otherClasses: string }) => {
+  const allClasses = classCombiner(['-ml-3 z-10', otherClasses])
+  
+ return <button type='submit' className={allClasses}>
+    <Image
+      src={"/magnifying-glass.svg"}
+      alt={"magnifying glass"}
+      width={40}
+      height={40}
+      className='object-contain'
+    />
+  </button>
+};
   
  
