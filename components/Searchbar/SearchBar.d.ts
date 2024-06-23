adb2dd0f712cd65a8dabf0d22ff4ed0782eb1c67
mehran <mohammadmoallemi78@gmail.com>
2024-06-23 22:06:48 +0330

    add SearchBarContextTypes

---
+++ components/Searchbar/SearchBar.d.ts
@@ -0,0 +1,8 @@
+
+interface SearchBarContextType {
+    manufacturer: string;
+    setManuFacturer: React.Dispatch<React.SetStateAction<string>>;
+    model: string;
+    setModel: React.Dispatch<React.SetStateAction<string>>;
+  }
+  
