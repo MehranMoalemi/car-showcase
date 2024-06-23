52c1139959f1c1c7d2fdd283e42d6511a7fffbff
mehran <mohammadmoallemi78@gmail.com>
2024-06-23 22:07:07 +0330

    add ShowMore Component

--- components/ShowMore/index.tsx
+++ components/ShowMore/index.tsx
@@ -2,22 +2,24 @@
 
 import { useRouter } from "next/navigation";
 
-import { updateSearchParams } from "@/utils";
+import { updateFilterSearchParams } from "@/utils";
 import { CustomButton } from "../CustomButton";
+import { useCallback } from "react";
 
 const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
   const router = useRouter();
 
-  const handleNavigation = () => {
+  const handleNavigation = useCallback(() => {
     // Calculate the new limit based on the page number and navigation type
     const newLimit = (pageNumber + 1) * 10;
 
     // Update the "limit" search parameter in the URL with the new value
-    const newPathname = updateSearchParams("limit", `${newLimit}`);
+    const newPathname = updateFilterSearchParams("limit", `${newLimit}`);
     
     router.push(newPathname);
-  };
+  }, [pageNumber, router]);
 
+  
   return (
     <div className="w-full flex-center gap-5 mt-10">
       {!isNext && (
